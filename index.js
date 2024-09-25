a54604c30a41b5cf3c81201483176d9eb56a78ed
Adrian Hinds <adrianhinds@adrians-mbp.mynetworksettings.com>
2024-09-24 23:01:24 -0400

    added middleware and styles.css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const CheckTime = require("../middleware/Checktime")
 
 const users = require("./data/users");
 const posts = require("./data/posts");
